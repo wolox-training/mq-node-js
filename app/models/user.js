c52f934ee92f9b3b8503348cb1ad66a48a610f20
Martin Queija <martin.queija@wolox.com.ar>
2018-11-07 12:32:18 -0300

    Adds user model

---
+++ app/models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, unique: true, allowNull: false },
+    firstName: { type: DataTypes.STRING, autoIncrement: true, allowNull: false },
+    lastName: { type: DataTypes.STRING, autoIncrement: true, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
---
+++ migrations/migrations/20181107143100-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password:{
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
