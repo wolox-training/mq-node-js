34b1b2ac41691241adcc5ff9be7448834b9b99e1
Martin Queija <martin.queija@wolox.com.ar>
2018-11-07 12:43:26 -0300

    fix, first and last name are not auto incrementable

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,16 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, unique: true, allowNull: false },
-    firstName: { type: DataTypes.STRING, autoIncrement: true, allowNull: false },
-    lastName: { type: DataTypes.STRING, autoIncrement: true, allowNull: false },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, unique: true, allowNull: false },
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, unique: true, allowNull: false }
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
