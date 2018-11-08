e3d9c34c61112b454bebf25cc923a7ebd18ec65f
Martin Queija <martin.queija@wolox.com.ar>
2018-11-08 11:23:36 -0300

    User signUp POST method implementation

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
@@ -9,10 +11,23 @@ module.exports = (sequelize, DataTypes) => {
       email: { type: DataTypes.STRING, unique: true, allowNull: false },
       password: { type: DataTypes.STRING, allowNull: false }
     },
-    { paranoid: true, underscored: true, timestamps: true }
+    {
+      paranoid: true,
+      underscored: true,
+      timestamps: true
+    }
   );
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  User.generateHash = function(password) {
+    return bcrypt.hash(password, bcrypt.genSaltSync(8));
+  };
+
+  User.validPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+  };
+
   return User;
 };
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
-// const controller = require('./controllers/controller');
+const userSignUpController = require('./controllers/userSignUp');
 
 exports.init = app => {
+  app.post('/users', userSignUpController.methodPOST);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
