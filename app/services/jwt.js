b2cfceabf5a4373a7d673805acd72b4ad8b8626f
tinoq-woloxer <44779707+tinoq-woloxer@users.noreply.github.com>
2018-11-20 12:16:48 -0300

    User admin (#8)
    * add boolean flag isAdmin to model User
    
    * isAdmin flag should be specified upon creation
    
    * user admin implementation and tests
    
    * package json was missing chai-subset dev dependency
    
    * remove brackets and return
    
    * use update instead of save to persist existing db object
    
    * use update instead of save on exising db object
    
    * remove brackets and return
    
    * use deconstruct
    
    * fix migration and change not authorized error message
    
    * move admin tests to user tests
    
    * move getUserForToken to jwt token service

--- app/services/jwt.js
+++ app/services/jwt.js
@@ -1,5 +1,16 @@
 const jwtsimple = require('jwt-simple'),
-  secret = require('../../config/index').common.session.secret;
+  secret = require('../../config/index').common.session.secret,
+  User = require('../models').User,
+  errors = require('../errors');
 
 exports.encode = payload => jwtsimple.encode(payload, secret);
 exports.decode = token => jwtsimple.decode(token, secret);
+
+exports.getUserForToken = token => {
+  try {
+    const { email } = exports.decode(token);
+    return User.find({ where: { email } });
+  } catch (e) {
+    throw errors.badRequest('Invalid Token');
+  }
+};
