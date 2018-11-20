37cc44846ed407d429974f49be2420f6b0a4b843
Martin Queija <martin.queija@wolox.com.ar>
2018-11-20 15:58:17 -0300

    always fetch albums instead of caching them

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -4,19 +4,10 @@ const jwt = require('../services/jwt'),
   albumsUri = 'https://jsonplaceholder.typicode.com/albums',
   errors = require('../errors');
 
-let albums;
-
 const getAlbums = () => {
-  if (albums) return Promise.resolve(albums);
-  // lazy initialization
-  return request({ uri: albumsUri, json: true })
-    .then(res => {
-      albums = res;
-      return albums;
-    })
-    .catch(e => {
-      throw errors.resourceNotFound('albums not available');
-    });
+  return request({ uri: albumsUri, json: true }).catch(e => {
+    throw errors.resourceNotFound('albums not available');
+  });
 };
 
 exports.listAlbums = (req, res, next) =>
