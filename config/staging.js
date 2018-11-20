ecedde5093383001f39debb20101c5b68eeffe02
Martin Queija <martin.queija@wolox.com.ar>
2018-11-20 14:19:41 -0300

    overwrite app port when in staging with process.env.PORT

--- config/staging.js
+++ config/staging.js
@@ -1,5 +1,8 @@
 exports.config = {
   environment: 'staging',
   isStage: true,
-  use_env_variable: 'DATABASE_URL'
+  use_env_variable: 'DATABASE_URL',
+  common: {
+    port: process.env.PORT
+  }
 };
