790f67907eaed0956190e8c01b4a7e9364496f64
Martin Queija <martin.queija@wolox.com.ar>
2018-11-27 10:29:46 -0300

    defaultValue: null is unnecesary

--- migrations/migrations/20181123185524-addUserMostRecentValidTokenTimestamp.js
+++ migrations/migrations/20181123185524-addUserMostRecentValidTokenTimestamp.js
@@ -4,7 +4,6 @@ module.exports = {
   up: (queryInterface, Sequelize) =>
     queryInterface.addColumn('Users', 'oldestTokenTimestamp', {
       allowNull: true,
-      defaultValue: null,
       type: Sequelize.DATE
     }),
   down: (queryInterface, Sequelize) => queryInterface.removeColumn('Users', 'oldestTokenTimestamp')
